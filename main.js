ca061d1c1ef40aa001525cef9cbf12a900f7832e
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-19 14:24:37 +0530

    List Rendering

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ const app = Vue.createApp({
       url: "https://www.vuemastery.com/",
       inventory: 100,
       onSale: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      sizes: ["S", "M", "L", "XL"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+        },
+        {
+          id: 2235,
+          color: "blue",
+        },
+      ],
     };
   },
 });
