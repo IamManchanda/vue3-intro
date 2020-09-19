fe07993060ced603086d678905ed03750287c306
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-19 15:08:00 +0530

    Event Handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
       url: "https://www.vuemastery.com/",
@@ -12,12 +13,27 @@ const app = Vue.createApp({
         {
           id: 2234,
           color: "green",
+          image: "./assets/images/socks_green.jpg",
         },
         {
           id: 2235,
           color: "blue",
+          image: "./assets/images/socks_blue.jpg",
         },
       ],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart >= 1) {
+        this.cart -= 1;
+      }
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
